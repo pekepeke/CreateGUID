77a57b0e5aeac5bdabdae3c405fd3cd5a4cb7cd6
pekepeke <pekepekesamurai@gmail.com>
2012-10-14 22:56:31 +0900

    initial commit

---
+++ CreateGUID/CreateGUID.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class CreateGUID
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.txtGuid = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(205, 12);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 0;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // txtGuid
+            // 
+            this.txtGuid.Location = new System.Drawing.Point(12, 81);
+            this.txtGuid.Name = "txtGuid";
+            this.txtGuid.ReadOnly = true;
+            this.txtGuid.Size = new System.Drawing.Size(268, 19);
+            this.txtGuid.TabIndex = 1;
+            this.txtGuid.Click += new System.EventHandler(this.txtGuid_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(205, 42);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "New GUID";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Result";
+            // 
+            // CreateGUID
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 115);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtGuid);
+            this.Controls.Add(this.btnCopy);
+            this.Name = "CreateGUID";
+            this.Text = "CreateGUID";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.TextBox txtGuid;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CreateGUID/CreateGUID.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class CreateGUID : Form
+    {
+        public CreateGUID() {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e) {
+            Guid guid = Guid.NewGuid();
+            txtGuid.Text = guid.ToString();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e) {
+            String guid = txtGuid.Text;
+            // MessageBox.Show(guid);
+            if (guid.Length > 0) {
+                Clipboard.SetText(guid);
+            }
+        }
+
+        private void txtGuid_Click(object sender, EventArgs e) {
+            txtGuid.SelectAll();
+        }
+
+    }
+}
